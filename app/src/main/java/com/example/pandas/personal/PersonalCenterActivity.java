fa04837bbd93d31c01c92d834fda9ec7169fc41d
15239206484 <3143107187@qq.com>
2017-07-13 11:49:27 +0800

    李琰第一次提交

---
+++ app/src/main/java/com/example/pandas/personal/PersonalCenterActivity.java
@@ -0,0 +1,54 @@
+package com.example.pandas.personal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.example.pandas.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class PersonalCenterActivity extends AppCompatActivity {
+
+    @Bind(R.id.fanhui)
+    ImageButton fanhui;
+    @Bind(R.id.personalCenter_Signin)
+    LinearLayout personalCenterSignin;
+    @Bind(R.id.personalCenter_History)
+    LinearLayout personalCenterHistory;
+    @Bind(R.id.personalCenter_Collection)
+    LinearLayout personalCenterCollection;
+    @Bind(R.id.personalCenter_set)
+    LinearLayout personalCenterSet;
+    @Bind(R.id.activity_personal_center)
+    LinearLayout activityPersonalCenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_center);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.personalCenter_Signin, R.id.personalCenter_History, R.id.personalCenter_Collection, R.id.personalCenter_set, R.id.activity_personal_center})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.personalCenter_Signin:
+                startActivity(new Intent(PersonalCenterActivity.this,LoginActivity.class));
+                break;
+            case R.id.personalCenter_History:
+                break;
+            case R.id.personalCenter_Collection:
+                break;
+            case R.id.personalCenter_set:
+                break;
+            case R.id.activity_personal_center:
+                break;
+        }
+    }
+}
