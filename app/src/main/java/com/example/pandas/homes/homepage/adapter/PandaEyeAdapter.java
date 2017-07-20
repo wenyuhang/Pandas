14388228e4f18b26ccaf488320ec361772ef64ef
WZMr <1369671157@qq.com>
2017-07-20 14:46:16 +0800

    王泽 首页所有点击跳转详情页面进行视频播放

--- app/src/main/java/com/example/pandas/homes/homepage/adapter/PandaEyeAdapter.java
+++ app/src/main/java/com/example/pandas/homes/homepage/adapter/PandaEyeAdapter.java
@@ -1,27 +1,34 @@
 package com.example.pandas.homes.homepage.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.pandas.R;
+import com.example.pandas.config.CultureSpActivity;
+import com.example.pandas.model.biz.IHomeImpl;
 import com.example.pandas.model.datebean.homebean.PandaEyeListBean;
+import com.example.pandas.model.datebean.homebean.VideoInfoBean;
+import com.example.pandas.networks.mycallbacks.NetCallbacks;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Nicky on 2017/7/13.
  */
+
 public class PandaEyeAdapter extends RecyclerView.Adapter {
 
     private Context context;
     private ArrayList<PandaEyeListBean.ListBean> list;
+    private ArrayList<VideoInfoBean.VideoBean.ChaptersBean> videoList;
 
     public PandaEyeAdapter(Context context, ArrayList<PandaEyeListBean.ListBean> list) {
         this.context = context;
@@ -51,6 +58,7 @@ public class PandaEyeAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        videoList=new ArrayList<>();
         MyViewHolder viewHolder= (MyViewHolder) holder;
         Glide.with(context).load(list.get(position).getImage()).into(viewHolder.pandaeye_newsimg);
         viewHolder.pandaeye_newstime.setText(list.get(position).getVideoLength());
@@ -59,7 +67,24 @@ public class PandaEyeAdapter extends RecyclerView.Adapter {
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, list.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+                HashMap<String,String> map=new HashMap<String, String>();
+                map.put("pid",list.get(position).getPid());
+                IHomeImpl.ihttp.get("http://vdn.apps.cntv.cn/api/getVideoInfoForCBox.do", map, new NetCallbacks<VideoInfoBean>() {
+                    @Override
+                    public void onSuccess(final VideoInfoBean videoInfoBean) {
+                        videoList.addAll(videoInfoBean.getVideo().getChapters());
+                        Intent intent=new Intent(context, CultureSpActivity.class);
+                        intent.putExtra("url",videoList.get(0).getUrl());
+                        intent.putExtra("title",list.get(position).getTitle());
+                        context.startActivity(intent);
+                        videoList.clear();
+                    }
+
+                    @Override
+                    public void onError(String errorMsg) {
+
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/pandas/homes/homepage/adapter/PandaWatchTitleAdapter.java
+++ app/src/main/java/com/example/pandas/homes/homepage/adapter/PandaWatchTitleAdapter.java
@@ -1,18 +1,23 @@
 package com.example.pandas.homes.homepage.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.pandas.R;
+import com.example.pandas.config.CultureSpActivity;
+import com.example.pandas.model.biz.IHomeImpl;
 import com.example.pandas.model.datebean.homebean.HomePageBean;
+import com.example.pandas.model.datebean.homebean.VideoInfoBean;
+import com.example.pandas.networks.mycallbacks.NetCallbacks;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Nicky on 2017/7/13.
@@ -21,6 +26,7 @@ public class PandaWatchTitleAdapter extends RecyclerView.Adapter {
 
     private Context context;
     private ArrayList<HomePageBean.DataBean.PandaeyeBean.ItemsBean> list;
+    private ArrayList<VideoInfoBean.VideoBean.ChaptersBean> videoList;
 
     public PandaWatchTitleAdapter(Context context, ArrayList<HomePageBean.DataBean.PandaeyeBean.ItemsBean> list) {
         this.context = context;
@@ -48,13 +54,31 @@ public class PandaWatchTitleAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        videoList=new ArrayList<>();
         MyViewHolder viewHolder= (MyViewHolder) holder;
         viewHolder.panda_watch_btn.setText(list.get(position).getBrief());
         viewHolder.panda_watch_title.setText(list.get(position).getTitle());
         viewHolder.panda_watch_title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, list.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+                HashMap<String,String> map=new HashMap<String, String>();
+                map.put("pid",list.get(position).getPid());
+                IHomeImpl.ihttp.get("http://vdn.apps.cntv.cn/api/getVideoInfoForCBox.do", map, new NetCallbacks<VideoInfoBean>() {
+                    @Override
+                    public void onSuccess(final VideoInfoBean videoInfoBean) {
+                        videoList.addAll(videoInfoBean.getVideo().getChapters());
+                        Intent intent=new Intent(context, CultureSpActivity.class);
+                        intent.putExtra("url",videoList.get(0).getUrl());
+                        intent.putExtra("title",list.get(position).getTitle());
+                        context.startActivity(intent);
+                        videoList.clear();
+                    }
+
+                    @Override
+                    public void onError(String errorMsg) {
+
+                    }
+                });
             }
         });
     }
